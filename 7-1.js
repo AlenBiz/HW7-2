f31b8891cd1467e8d6cd4f114aa65d2a8ea086ab
[AlenBiz] <[jobbizyaev@gmail.com]>
2022-11-02 07:43:24 +0100

    added a 7-1

---
+++ 7-1.js
@@ -0,0 +1,21 @@
+const postomat = [null, null, null, "4", null];
+console.log(postomat.includes("4"));
+
+let customer = {
+    balance: 100,
+    kodFromSms: "A001DFX0"
+
+};
+console.log(customer);
+
+function SayStatus (kodForPostomat) {
+    if (customer.kodFromSms == kodForPostomat)
+    {
+        console.log("Вы получили посылку из ячейки 4 и ваш счет составляет: 0 р.");
+    }
+};
+
+SayStatus ("A001DFX0");
+
+
+
